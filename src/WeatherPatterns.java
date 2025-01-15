440621480ce3c632a1b96ed2c8fe7558dec934e0
Josh Little <josh.little@menloschool.org>
2025-01-15 13:23:31 -0800

    Got the algorithm working, doesn't complete all the test cases and takes 2x as much time as the limit is for the efficient large case, but at least I know the idea of what I should be doing.

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -3,7 +3,7 @@
  * each day’s temperature is higher than on the previous day in that sequence.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Josh Little
  */
 
 public class WeatherPatterns {
@@ -15,8 +15,80 @@ public class WeatherPatterns {
      * @return the longest run of days with increasing temperatures
      */
     public static int longestWarmingTrend(int[] temperatures) {
+        int[] temps = temperatures;
+//        for (int e : temps)
+//            System.out.print(e + " ");
+//        System.out.println();
+//        System.out.println("--------------------------------------------------------------------------------");
+        int size = temperatures.length;
+
+
+        // Potential idea is to run through the entirety of the data, record it into a 2D map
+        // that stores the temperatures at the indicies and then their locations in a second
+        // array inside each of the indicies (or arrayList to be more specific). Then, go from
+        // temperature to temperature, finding the longest run with that as the starting temp.
+        // Only need to start at each temperature once, because there's no reason to skip the
+        // rest
+
+        // Maybe keep a duplicate map that stores whether or not a location has already been visited to limit the number of paths that
+        // can be produced from that location or keep a score and the number of potential things after it at that point that are bigger.
+
+
+        // The one condition where you would want to skip a number that's greater than the
+        // current value if it's not just the next greatest, would be if its present after the next greatest that's later on.
+
+        // For example, if the current temp was 56 degrees and there was 58 degrees right next to it, if
+        // there was a 57 later on with a 58 after it as well, then you could skip.
+
+        // Highest string below it that's below the actual number and then add +1
+        // create a map that stores the longest current run for specific values. For example,
+        // if there were the number 1 4 7 20 9, you would go that the max is 3 for 7
+        // so when you reach 20, the answer would be 4, but when you reach 9, it would also
+        // be 4 because the longest run less than 9 would be stored at 7 which is 3.
+        // 1 3 2 3 4
+
+
+        // Offset everything from -50 to 130 to start at 0
+        // Map that stores the greatest current run that ends with a temperature correlated to
+        // the index of the map with an offset of 50 to fit all temps from -50° to 130°.
+        int longestRun = 0;
+
+        for (int i = 0; i < size; i++) {
+            int[] map = new int[181];
+            // Iterate through the rest of the temperatures from the starting location i
+            for (int j = i; j < size; j++) {
+//                System.out.println("Current Number: " + j + " temp: " + temps[j]);
+                map[temps[j] + 50] = findCurrentRun(j, temps, map);
+//                System.out.println("--------------------------------------------------------------------------------");
+            }
+            // Iterate through the entirety of the current length map to check if any of the new
+            // longest values is greater than the current longestRun. If so, replace.
+            for (int num : map)
+                longestRun = num > longestRun ? num : longestRun;
+//            System.out.println("best run: " + longestRun);
+        }
+
         // TODO: Write your code here!
 
-        return 0;
+        return longestRun;
     }
+
+    public static int findCurrentRun(int end, int[] temps, int[] map) {
+//        System.out.print("Temperatures: ");
+        for (int i = 0; i < end; i++) {
+//            System.out.print(temps[i] + " | ");
+        }
+//        System.out.println();
+        int currentRun = 0;
+        int currentTemp = temps[end] + 50;
+//        System.out.println("Current Temp: " + currentTemp);
+        for (int i = 0; i < currentTemp; i++) {
+            if (map[i] > currentRun)
+                currentRun = map[i];
+        }
+//        System.out.println("current best run: " + (currentRun + 1));
+        return currentRun + 1;
+    }
+
+
 }
