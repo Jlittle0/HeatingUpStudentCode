f48612ca0c96b739f9f24ef27c599b541abcc720
Josh Little <josh.little@menloschool.org>
2025-01-23 12:28:25 -0800

    Final Submission

--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -16,11 +16,11 @@ public class WeatherPatterns {
      * @param temperatures
      * @return the longest run of days with increasing temperatures
      */
-    public static int[] map;
+    public static int[] currentRuns;
 
     public static int longestWarmingTrend(int[] temperatures) {
         // Initialize the map that stores current best run for each index
-        map = new int[temperatures.length];
+        currentRuns = new int[temperatures.length];
 
         // Create adjacencyList and fill it out with the indicies of lesser temperatures
         ArrayList<ArrayList<Integer>> adjacencyList = new ArrayList<>();
@@ -35,37 +35,37 @@ public class WeatherPatterns {
             adjacencyList.add(temp);
         }
 
-        int[] map = new int[temperatures.length];
         // For every element in the adjacencyList, iterate through it and find longest path.
         for (int i = 0; i < adjacencyList.size(); i++) {
-            findCurrentRun(i, i, adjacencyList, map);
+            findCurrentRun(i, i, adjacencyList, currentRuns);
         }
 
         // Go through all the best runs for each index, find the longest one, and then return it.
         int longestRun = 0;
-        for (int i : map)
+        for (int i : currentRuns)
             longestRun = i > longestRun ? i : longestRun;
 
         return longestRun;
     }
 
-    public static void findCurrentRun(int oIndex, int cIndex, ArrayList<ArrayList<Integer>> adjacencyList, int[] map) {
+    // Original index, current index, adjacencyList to find all lesser values, and map that stores longestRuns.
+    public static void findCurrentRun(int oIndex, int cIndex, ArrayList<ArrayList<Integer>> adjacencyList, int[] currentRuns) {
         // If the current index already has a calculated longestRun, check if it's longer than the
         // original index's longest or equal and if so, add one and make that original's longest.
-        if (map[cIndex] != 0) {
-            map[oIndex] = map[cIndex] >= map[oIndex] ? map[cIndex] + 1 : map[oIndex];
+        if (currentRuns[cIndex] != 0) {
+            currentRuns[oIndex] = currentRuns[cIndex] >= currentRuns[oIndex] ? currentRuns[cIndex] + 1 : currentRuns[oIndex];
             return;
         }
         // If the current index has no elements that are less than it and doesn't have a longest
         // run value already, set its longestRun value to 1.
         if (adjacencyList.get(cIndex).size() == 0) {
-            map[cIndex] = 1;
+            currentRuns[cIndex] = 1;
             return;
         }
 
         // Recurse on all elements that are less than the current index stores in adjacencyList.
         for (Integer i : adjacencyList.get(cIndex)) {
-            findCurrentRun(oIndex, i, adjacencyList, map);
+            findCurrentRun(oIndex, i, adjacencyList, currentRuns);
         }
     }
 }
